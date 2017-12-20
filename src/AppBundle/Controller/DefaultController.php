0ec7977846de02991b5585e59da6e7b95a6a221e
greeflas <vldmr.kuprienko@gmail.com>
2017-12-20 18:39:06 +0200

    Basics: routing

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -16,12 +16,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends Controller
 {
     /**
+     * Routing example.
+     * @see \Symfony\Component\Routing\Generator\UrlGeneratorInterface
+     *
+     * @return Response
+     *
+     * @Route("/some-page")
+     */
+    public function somePageAction()
+    {
+        $homePageUrl = $this->generateUrl('home_page');
+        return $this->render('default/some-page.php.twig', compact('homePageUrl'));
+    }
+
+    /**
      * Index action.
      *
      * @param string $username
      * @return Response
      *
-     * @Route("/{username}")
+     * @Route("/{username}", name="home_page")
      */
     public function indexAction(string $username = '') : Response
     {
